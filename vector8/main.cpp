94ce0abb2085cdc862cc0dbc012d561f5c466ebd
twomons <142531859+twomons@users.noreply.github.com>
2023-08-18 02:19:47 +0800

    Add files via upload

---
+++ vector8/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Vector.h"
+
+int main()
+{
+    // 创建一个空的Vector对象
+    Vector<int> vec1;
+
+    // 添加元素到Vector对象
+    vec1.push_back(1);
+    vec1.push_back(2);
+    vec1.push_back(3);
+
+    // 使用下标运算符访问元素
+    std::cout << "vec1[0]: " << vec1[0] << std::endl;
+    std::cout << "vec1[1]: " << vec1[1] << std::endl;
+    std::cout << "vec1[2]: " << vec1[2] << std::endl;
+
+    // 使用迭代器遍历Vector对象
+    std::cout << "vec1: ";
+    for (Vector<int>::iterator it = vec1.begin(); it != vec1.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    // 创建一个带有初始值的Vector对象
+    Vector<int> vec2(5, 10);
+    std::cout << "vec2: ";
+    for (Vector<int>::iterator it = vec2.begin(); it != vec2.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    // 复制Vector对象
+    Vector<int> vec3 = vec1;
+    std::cout << "vec3: ";
+    for (Vector<int>::iterator it = vec3.begin(); it != vec3.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    // 判断两个Vector对象是否相等
+    if (vec1 == vec3)
+    {
+        std::cout << "vec1 and vec3 are equal." << std::endl;
+    }
+    else
+    {
+        std::cout << "vec1 and vec3 are not equal." << std::endl;
+    }
+
+    // 清空Vector对象
+    vec1.clear();
+    std::cout << "vec1 size after clear: " << vec1.size() << std::endl;
+
+    return 0;
+}
